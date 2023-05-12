fbaa3348f593593fe6031f298ab469568b654898
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-05-12 13:59:55 +0200

    Starter files

---
+++ src/main/java/com/kabelo/tutorsrus/TutorsrusApplication.java
@@ -0,0 +1,13 @@
+package com.kabelo.tutorsrus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorsrusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorsrusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kabelo/tutorsrus/TutorsrusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kabelo.tutorsrus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorsrusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
