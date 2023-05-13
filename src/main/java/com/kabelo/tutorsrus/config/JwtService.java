67a502939ff758920c563246e4e1151203c62fe9
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-05-13 12:03:22 +0200

    extractAllClaims method ready to use. With KEY

--- src/main/java/com/kabelo/tutorsrus/config/JwtService.java
+++ src/main/java/com/kabelo/tutorsrus/config/JwtService.java
@@ -1,10 +1,33 @@
 package com.kabelo.tutorsrus.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Base64;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY ="6B58703273357638792F423F4528482B4D6250655368566D597133743677397A";
     public String extractUsername(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
