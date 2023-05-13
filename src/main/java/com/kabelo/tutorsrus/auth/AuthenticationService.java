ad6cef685b2a203fdd585808fd266080a8c3f0cf
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-05-13 19:16:57 +0200

    Implement RegisterRequest

---
+++ src/main/java/com/kabelo/tutorsrus/auth/AuthenticationService.java
@@ -0,0 +1,35 @@
+package com.kabelo.tutorsrus.auth;
+
+import com.kabelo.tutorsrus.config.JwtService;
+import com.kabelo.tutorsrus.user.Role;
+import com.kabelo.tutorsrus.user.User;
+import com.kabelo.tutorsrus.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        return null;
+    }
+}
