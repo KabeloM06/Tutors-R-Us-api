cb7da674a132800030da875381b944a0dc8c1141
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-05-13 19:24:13 +0200

    Implement AuthenticationRequest

--- src/main/java/com/kabelo/tutorsrus/auth/AuthenticationService.java
+++ src/main/java/com/kabelo/tutorsrus/auth/AuthenticationService.java
@@ -5,6 +5,8 @@ import com.kabelo.tutorsrus.user.Role;
 import com.kabelo.tutorsrus.user.User;
 import com.kabelo.tutorsrus.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,7 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder()
@@ -30,6 +33,16 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        //if user is authenticated
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
     }
 }
